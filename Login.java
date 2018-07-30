c6c909cf5bba43f0937791d096514fd762b79f49
nehasiwakoti <siwakotineha22@gmail.com>
2018-07-29 19:51:50 -0700

    login informtion

---
+++ Login.java
@@ -0,0 +1,24 @@
+public class Login{
+    private String username="Neha Siwakoti";
+	private String password;
+	  
+	public Login(){
+	}
+	
+	public String getUsername(){
+	    return this.username;
+	}
+	
+	public void setUsername(String un){
+		this.username = un;
+	}
+	
+	public String getPassword(){
+	    return this.password;
+	}
+	 public void setPassword(String pw){
+	     this.password = pw;
+		 }
+		 
+		
+}
