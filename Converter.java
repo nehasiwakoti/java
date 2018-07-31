109ad4bc280639391436ccd8ed6634e8155283a7
nehasiwakoti <siwakotineha22@gmail.com>
2018-07-30 18:29:39 -0700

    converter

---
+++ Converter.java
@@ -0,0 +1,17 @@
+public class Converter{
+    public static void main(String[] args){
+	String value ="Lata Paudel,Neha Siwakoti";
+	//int a=Integer.parseInt(value);
+	
+	String a=value.substring(0,3);
+	System.out.println(a);
+	
+	String s=value.substring(3,6);
+	System.out.println(s);
+	
+	
+	
+	}
+	} 
+	
+	
