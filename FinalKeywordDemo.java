1cf980c99dd177bfdab1d58a24fa8c2493e97385
nehasiwakoti <siwakotineha22@gmail.com>
2018-07-30 18:15:22 -0700

    keyword

---
+++ FinalKeywordDemo.java
@@ -0,0 +1,15 @@
+public class FinalKeywordDemo{
+      public static void main(String[] args){
+	   // final  String name="Siwakoti";
+		// name="Baral";
+		  //  System.out.println("Name is:"+name);
+		  
+		  FinalA a=new FinalA();
+		  a.printHello();
+		  
+		  FinalB b=new FinalB();
+		  b.printHello();
+			}
+			}
+		 
+	     
