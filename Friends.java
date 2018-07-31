3404b185b1cb57930c24a9cb6ee9fa707ec593ff
nehasiwakoti <siwakotineha22@gmail.com>
2018-07-30 18:36:34 -0700

    friend

---
+++ Friends.java
@@ -0,0 +1,8 @@
+ INSERT INTO information (sn,name,address,phone_num,faculty,semester) 
+ VALUES (1,'Neha Baral','Bhaktapur','9860905670','csit','3rd'),
+ (2,'Sujata thapa','Baneshwor','9856789789','csit','3rd'),
+ (3,Neha Bhandari','chahabil','9876543210','csit','3rd'),
+ (4,'Dipa Rajbansi','Baneshwor','9876543056','csit','3rd'),
+ (5,'Isha uprety','Itahari','9876543250','csit','third');
+    
+     
